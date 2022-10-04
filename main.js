15ab8a4b30d6b21db085f71923b07166da1bf851
tunjay-h <tuncayhabibbeyli@gmail.com>
2022-10-04 12:59:23 +0400

    work on second number

--- main.js
+++ main.js
@@ -1,3 +1,5 @@
+// Pure function - no dom query, no dom manipulation
+// same result if same inputs are passed 
 function convertDecimalToBinary(number, bitCount) {
   let bits = [];
   let isNegative = number < 0;
@@ -17,18 +19,19 @@ function convertDecimalToBinary(number, bitCount) {
   if (isNegative) {
     let initialLength = bits.length;
     for (let index = 0; index < bitCount - initialLength; index++) {
-      bits.unshift(0); // insert at the beginning 
+      bits.unshift(0); // insert at the beginning
     }
-    return bits.map(elem => (elem + 1) % 2); // map(elem => elem ^ 1) // xor
+    return bits.map((elem) => (elem + 1) % 2); // map(elem => elem ^ 1) // xor
   }
   return bits;
 }
 
+// Pure
 function convertDecimalToBinaryPowers(number, bitCount) {
   let counter = 0;
   let powersOf2 = [];
 
-  while (1 << counter <= Math.abs(number) || (counter + 1) <= bitCount) {
+  while (1 << counter <= Math.abs(number) || counter + 1 <= bitCount) {
     powersOf2.push(number & (1 << counter));
     counter++;
   }
@@ -36,79 +39,149 @@ function convertDecimalToBinaryPowers(number, bitCount) {
   return powersOf2.reverse();
 }
 
+// pure
 const numberBoxComponent = (number) => {
-    // let div = document.createElement("div");
-    return `<div style='min-width: 3rem; height: 3rem; border: 1px solid black; border-radius: 0.25rem; margin: 2px 2px;'>${number}</div>`;
+  // let div = document.createElement("div");
+  return `<div style='min-width: 3rem; height: 3rem; border: 1px solid black; border-radius: 0.25rem; margin: 2px 2px;'>${number}</div>`;
 };
 
+// pure
 const numbersOf2Component = (numbers) => {
-    let divBody = '';
-    numbers.map((elem, index) => {
-        divBody += numberBoxComponent(elem);
-    });
-    return `<div style='display: flex;'>${divBody}</div>`;
+  let divBody = "";
+  numbers.map((elem, index) => {
+    divBody += numberBoxComponent(elem);
+  });
+  return `<div style='display: flex;'>${divBody}</div>`;
 };
 
+// pure
 const bitBoxComponent = (number) => {
   return `<div><input type='number' placeholder='0' style='width: 30px; height: 30px;' value='${number}' /></div>`;
 };
 
+// pure
 const allBitBoxesComponent = (numbers) => {
-  let divBody = '';
+  let divBody = "";
   numbers.map((elem, index) => {
-      if (index % 8 === 0) {
-        divBody += `<div style='width: 30px; height: 30px;'></div>`;
-      }
-      divBody += bitBoxComponent(elem);
+    if (index % 8 === 0) {
+      divBody += `<div style='width: 30px; height: 30px;'></div>`;
+    }
+    divBody += bitBoxComponent(elem);
   });
   return `<div style='display: flex;'>${divBody}</div>`;
 };
 
 // after document onload
 (() => {
-    const firstNumber = document.getElementById("firstNumber");
-    const resultInPowersOf2 = document.getElementById("resultInPowersOf2");
+  const firstNumber = document.getElementById("firstNumber");
+  const sumAsPowersOf2_first = document.getElementById("sumAsPowersOf2_first");
+  const secondNumber = document.getElementById("secondNumber");
+  const sumAsPowersOf2_second = document.getElementById("sumAsPowersOf2_second");
 
-    firstNumber.addEventListener('input', (event) => {
-        // firstNumber.value
-        if (event.target.value) {
-            let bitCount = document.getElementById("numberOfBoxes").value; // 8-bit
-            resultInPowersOf2.innerText = `= ${[...convertDecimalToBinaryPowers(event.target.value, bitCount)].join(" + ")}`;
+  const numberOfBoxes = document.getElementById("numberOfBoxes");
 
-            let data = convertDecimalToBinaryPowers(event.target.value, bitCount); // 16 0 4 0 0
-            let renderResult = numbersOf2Component(data);                
+  const renderNumbersOf2 = (number, boxCount) => {
+    let data = convertDecimalToBinaryPowers(number, boxCount); // 16 0 4 0 0
+    let renderResult = numbersOf2Component(data);
 
-            let indices = Array.from(data.keys()).reverse();                       // 0 1 2 3 4  =>  4 3 2 1 0
-            indices = indices.map(elem => `2<sup>${elem}</sup>`);        // 2^0  2^1  2^2  2^3 ....
-            let renderIndices = numbersOf2Component(indices);
+    let indices = Array.from(data.keys()).reverse(); // 0 1 2 3 4  =>  4 3 2 1 0
+    indices = indices.map((elem) => `2<sup>${elem}</sup>`); // 2^0  2^1  2^2  2^3 ....
+    let renderIndices = numbersOf2Component(indices);
 
-            document.getElementById("result").innerHTML = renderResult + renderIndices;
-        } else {
-            resultInPowersOf2.innerText = "";
-            document.getElementById("result").innerHTML = "";
-        }
-    });
+    return [renderResult, renderIndices];
+  };
 
-    firstNumber.addEventListener('input', (event) => {
-      // firstNumber.value
-      if (event.target.value) {
-        let bitCount = document.getElementById("numberOfBoxes").value; // 8-bit  =>  0 0 0 1 0 1 0 0
-        let data = convertDecimalToBinary(event.target.value, bitCount); //  1 0 1 0 0
+  // update powers of 2 and numbers of 2
+  firstNumber.addEventListener("input", (event) => {
+    // firstNumber.value
+    if (event.target.value) {
+      let bitCount = numberOfBoxes.value; // 8-bit
+      sumAsPowersOf2_first.innerText = `= ${[
+        ...convertDecimalToBinaryPowers(event.target.value, bitCount),
+      ].join(" + ")}`;
 
-        if (data.length <= bitCount) {
-          let initialLength = data.length;
-          for (let index = 0; index < bitCount - initialLength; index++) {
-            data.unshift(0); // insert at the beginning 
-          }
-        } else {
-          data.splice(0, data.length - bitCount); // remove from beginning
-        }
-        let renderBitBoxes = allBitBoxesComponent(data);
+      let [renderResult, renderIndices] = renderNumbersOf2(
+        event.target.value,
+        bitCount
+      );
 
-        document.getElementById("bitBoxes").innerHTML = renderBitBoxes;
-      } else {
-          document.getElementById("bitBoxes").innerHTML = "";
-      }
+      document.getElementById("result_first").innerHTML =
+        renderResult + renderIndices;
+    } else {
+      sumAsPowersOf2_first.innerText = "";
+      document.getElementById("result_first").innerHTML = "";
+    }
   });
+
+  secondNumber.addEventListener("input", (event) => {
+    // firstNumber.value
+    if (event.target.value) {
+      let bitCount = numberOfBoxes.value; // 8-bit
+      sumAsPowersOf2_second.innerText = `= ${[
+        ...convertDecimalToBinaryPowers(event.target.value, bitCount),
+      ].join(" + ")}`;
+
+      let [renderResult, renderIndices] = renderNumbersOf2(
+        event.target.value,
+        bitCount
+      );
+
+      document.getElementById("result_second").innerHTML =
+        renderResult + renderIndices;
+    } else {
+      sumAsPowersOf2_second.innerText = "";
+      document.getElementById("result_second").innerHTML = "";
+    }
+  });
+  
+
+  const renderAllBitBoxes = (number, boxCount) => {
+    let data = convertDecimalToBinary(number, boxCount); //  1 0 1 0 0
+
+    if (data.length <= boxCount) {
+      let initialLength = data.length;
+      for (let index = 0; index < boxCount - initialLength; index++) {
+        data.unshift(0); // insert at the beginning
+      }
+    } else {
+      data.splice(0, data.length - boxCount); // remove from beginning
+    }
+    let renderBitBoxes = allBitBoxesComponent(data);
+
+    return renderBitBoxes;
+  };
+
+  // update bit boxes
+  firstNumber.addEventListener("input", (event) => {
+    // firstNumber.value
+    if (event.target.value) {
+      let bitCount = numberOfBoxes.value; // 8-bit  =>  0 0 0 1 0 1 0 0
+
+      let renderBitBoxes = renderAllBitBoxes(event.target.value, bitCount);
+
+      document.getElementById("bitBoxes").innerHTML = renderBitBoxes;
+    } else {
+      document.getElementById("bitBoxes").innerHTML = "";
+    }
+  });
+
+  // update all results
+  numberOfBoxes.addEventListener("change", (event) => {
+    let bitCount = event.target.value;
     
+    sumAsPowersOf2_first.innerText = `= ${[
+      ...convertDecimalToBinaryPowers(firstNumber.value, bitCount),
+    ].join(" + ")}`;
+
+    // second one todo
+
+    let [renderResult, renderIndices] = renderNumbersOf2(
+      firstNumber.value,
+      bitCount
+    );
+    document.getElementById("result").innerHTML = renderResult + renderIndices;
+
+    let renderBitBoxes = renderAllBitBoxes(firstNumber.value, bitCount);
+    document.getElementById("bitBoxes").innerHTML = renderBitBoxes;
+  });
 })();
